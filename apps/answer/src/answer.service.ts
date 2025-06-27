8f080877ece26bcccf58c6f84d289f1adf542a9b
zhenghui-su <343196323@qq.com>
2025-06-27 14:21:15 +0800

    feat(answer): 添加答题功能和数据导出功能
    - 新增答题添加、列表、查找等功能
    - 实现答题数据导出为 Excel 文件
    - 添加全局验证管道
    - 新增 Excel 服务模块

--- apps/answer/src/answer.service.ts
+++ apps/answer/src/answer.service.ts
@@ -1,8 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@app/prisma';
+import { Inject, Injectable } from '@nestjs/common';
+import { AnswerAddDto } from './dto/answer-add.dto';
 
 @Injectable()
 export class AnswerService {
+  @Inject(PrismaService)
+  private prismaService: PrismaService;
   getHello(): string {
     return 'Hello World!';
+  }
+  async add(dto: AnswerAddDto, userId: number) {
+    return this.prismaService.answer.create({
+      data: {
+        content: dto.content,
+        score: 0,
+        answerer: {
+          connect: { id: userId },
+        },
+        exam: {
+          connect: { id: dto.examId },
+        },
+      },
+    });
+  }
+  async list(examId: number) {
+    return this.prismaService.answer.findMany({
+      where: { examId },
+      include: {
+        exam: true,
+        answerer: true,
+      },
+    });
+  }
+  async find(id: number) {
+    return this.prismaService.answer.findUnique({
+      where: { id },
+      include: {
+        exam: true,
+        answerer: true,
+      },
+    });
   }
 }
--- apps/answer/src/main.ts
+++ apps/answer/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AnswerModule } from './answer.module';
 import { Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AnswerModule);
@@ -10,8 +11,10 @@ async function bootstrap() {
       port: 8888,
     },
   });
-  await app.startAllMicroservices();
+  // await app.startAllMicroservices();
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(process.env.port ?? 3003);
 }
 bootstrap();
